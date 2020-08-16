f8f8f1d6266528119fb3deeb779258b70ffe3237
yiyuankafei <124633367@qq.com>
2020-08-16 20:48:17 +0800

    注释dubbo相关

--- src/main/java/com/yiyuankafei/wx/athena/dubbo/impl/TestServiceImpl.java
+++ src/main/java/com/yiyuankafei/wx/athena/dubbo/impl/TestServiceImpl.java
@@ -1,10 +1,8 @@
 package com.yiyuankafei.wx.athena.dubbo.impl;
 
-import org.apache.dubbo.config.annotation.Service;
-
 import com.yiyuankafei.wx.athena.dubbo.TestService;
 
-@Service(version = "1.0.0", group = "wx-athena-group", timeout = 60000, retries = -1)
+//@Service(version = "1.0.0", group = "wx-athena-group", timeout = 60000, retries = -1)
 public class TestServiceImpl implements TestService {
 	
 	@Override
